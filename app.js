69bffb7854644170a4530e4e7bd3801a2dc75175
AFAvella <aavella62419@universidadean.edu.co>
2023-07-19 10:59:55 -0500

    Git con mensaje de bienvenida

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.sendFile('codigo/pruebaCodigo.js', {root: __dirname});
+  res.send("Mensaje de bienvenida al IBM Coninuous Delivery con tekton");
 });
 
 app.listen(PORT);
